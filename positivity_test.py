1dd37b4e1fd79924f153f018b38a5f7077349e26
Marcel Suter <marcel.suter@bzz.ch>
2023-07-20 10:25:56 +0200

    initial commit

---
+++ positivity_test.py
@@ -0,0 +1,16 @@
+from positivity import main
+
+
+def test_even(capsys, monkeypatch):
+    inputs = iter([18])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'Number 18 is even\n'
+
+def test_odd(capsys, monkeypatch):
+    inputs = iter([37])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'Number 37 is odd\n'
