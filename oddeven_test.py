ff9d3ca5d91576a00b335c5cf7cc60303fcd48dd
graphics80 <K.Ilkow@gmail.com>
2023-10-03 08:59:31 +0200

    Update oddeven_test.py

--- oddeven_test.py
+++ oddeven_test.py
@@ -2,14 +2,14 @@ from oddeven import main
 
 
 def test_even(capsys, monkeypatch):
-    inputs = iter([18])
+    inputs = iter(['18'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
     assert captured.out == 'Number 18 is even\n'
 
 def test_odd(capsys, monkeypatch):
-    inputs = iter([37])
+    inputs = iter(['37'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
